45bd54ceb703c94aba0e8413406f864ce01fe203
amilr <amil.reyes@gmail.com>
2018-11-26 14:50:08 +0800

    Initial commit

---
+++ SimpleLauncher/MainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SimpleLauncher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbItems = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbItems
+            // 
+            this.lbItems.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbItems.FormattingEnabled = true;
+            this.lbItems.ItemHeight = 25;
+            this.lbItems.Location = new System.Drawing.Point(12, 12);
+            this.lbItems.Name = "lbItems";
+            this.lbItems.Size = new System.Drawing.Size(322, 354);
+            this.lbItems.TabIndex = 0;
+            this.lbItems.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbItems_KeyDown);
+            this.lbItems.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbItems_MouseDoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 379);
+            this.Controls.Add(this.lbItems);
+            this.Name = "MainForm";
+            this.Text = "Simple Launcher";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbItems;
+    }
+}
+
---
+++ SimpleLauncher/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SimpleLauncher
+{
+    public partial class MainForm : Form
+    {
+        private List<KeyValuePair<string, string>> Commands = new List<KeyValuePair<string, string>>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var xdoc = XDocument.Load("config.xml");
+
+            foreach (var item in xdoc.Document.Root.Elements("Item"))
+            {
+                var label = item.Element("Label").Value;
+                var command = item.Element("Command").Value;
+
+                Commands.Add(new KeyValuePair<string, string>(label, command));
+            }
+
+            foreach (var command in Commands)
+            {
+                lbItems.Items.Add(command.Key);
+            }
+        }
+
+        private void lbItems_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = lbItems.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                var command = Commands[index].Value;
+
+                Process.Start(command);
+            }
+        }
+
+        private void lbItems_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var command = Commands[lbItems.SelectedIndex].Value;
+
+                Process.Start(command);
+            }
+        }
+    }
+}
